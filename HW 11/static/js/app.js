5f8ab0449b26a8c7af747cab6f82c8cd0f5d5410
Brian_B <brianbarragan412@gmail.com>
2019-03-10 19:58:30 -0700

    update

--- HW 11/static/js/app.js
+++ HW 11/static/js/app.js
@@ -5,9 +5,9 @@ var tableData = data;
 
 var table = d3.select("tbody");
 
-function dataMe (arr) {
+function dataMe (array) {
 
-    data.forEach(function(item){
+array.forEach(function(item){
    var row = table.append("tr");
    Object.entries(item).forEach(function([key, value]){
     var cell = row.append("td")
@@ -20,35 +20,32 @@ dataMe(tableData);
 
 //filter data
 
+function handleClick(){
+
 d3.select("#filter-btn").on("click", function() {
 
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
+// Prevent the page from refreshing
+d3.event.preventDefault();
 
-    // remove old table data
-    $("tbody").children().remove()
-  
-    // Get the value property of the input element
-    var inputValue = d3.select("#datetime").property("value");
-  
-    // Create Filtered dataset based on InputValue entered by user
-    if (inputValue) {
-    var filterdata = tableData.filter(onerec => onerec.datetime === inputValue);}
-   
-    // Build new UFO Table with the filtered subset of UFO Sighting data
-
-    dataMe(filterdata);
-  });
-
-// Aash's blueprint
-// function handleClick(){
-//     d3.select("#filter-btn").on("click", function(){
-//     d3.event.preventDefault();
-//     text = d3.select("#datetime").property("value");
-//     $("#table_of_items tbody tr").remove()
-//     return dataMe(tableData)
-// })
-// }
+// remove old table data
+d3.selectAll('td').remove();
 
 
-// $("#table_of_items tbody tr").remove() removed body of table
+// Get the value property of the input element
+var inputValue = d3.select("#datetime").property("value");
+console.log(inputValue);
+
+// Create Filtered dataset based on InputValue entered by user
+var filterData = tableData.filter(item => item.datetime === inputValue);
+console.log(filterData);
+
+
+// Build new UFO Table with the filtered subset of UFO Sighting data
+
+dataMe(filterData);
+
+});
+};
+
+handleClick();
+
