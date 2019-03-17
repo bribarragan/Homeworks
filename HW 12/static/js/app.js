e90ce0e7a8541e91248c081204f0206b1aac35d3
Brian_B <brianbarragan412@gmail.com>
2019-03-17 16:12:40 -0700

    new hw

---
+++ HW 12/static/js/app.js
@@ -0,0 +1,95 @@
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+
+  // Use `d3.json` to fetch the metadata for a sample
+    // Use d3 to select the panel with id of `#sample-metadata`
+    var MetaData = `/metadata/${sample}`;
+    d3.json(MetaData).then(function(response) {
+   
+ 
+    // Use `.html("") to clear any existing metadata
+    var panelData = d3.select("#sample-metadata");
+    panelData.html("");
+
+    // Use `Object.entries` to add each key and value pair to the panel
+    var data = Object.entries(response);
+    console.log(data);
+    data.forEach(function(item) {
+    panelData.append("div").text(item[0]+ " : " + item[1])
+   });
+   })
+  };
+    // Hint: Inside the loop, you will need to use d3 to append new
+    // tags for each key-value in the metadata.
+
+
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var sampleData = `/samples/${sample}`;
+    // @TODO: Build a Bubble Chart using the sample data
+
+    d3.json(sampleData).then(function(response){
+      var Ids = response.otu_ids;
+      var sampleValues = response.sample_values;
+      var labels = response.otu_labels
+
+    bubbleData = [{
+      x: Ids,
+      y: sampleValues,
+      text: labels,
+      mode: "markers",
+      marker: {
+        color: Ids,
+        size: sampleValues
+    }
+    }]; 
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+
+      var topTenIds = response.otu_ids.slice(0,10);
+      var topTenLabels = response.otu_labels.slice(0,10);
+      var topTenSampleValues = response.sample_values.slice(0,10);
+  
+      var pieData = [{
+        "labels": topTenIds,
+        "values": topTenSampleValues,
+        "hovertext": topTenLabels,
+        "type": "pie"
+      }];
+      
+Plotly.newPlot('pie', pieData);
+Plotly.newPlot("bubble", bubbleData)
+});
+};
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+};
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+};
+
+// Initialize the dashboard
+init();
